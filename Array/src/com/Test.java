696a6a2e7a9825d8e5bb41a6084afd0571ae5812
PallaviMhaske11 <pallavimhaske2530@gmail.com>
2025-09-21 22:19:06 +0530

    add array project by TL

---
+++ Array/src/com/Test.java
@@ -0,0 +1,25 @@
+package com;
+
+public class Test 
+{
+	public static void main(String [] args)
+	{
+	 int []arr={10,20,30,40,50,60,70};
+	 System.out.println(arr);
+
+	 int fifthIndex=arr[5];
+	 System.out.println(fifthIndex);
+
+	 int[]ar=new int[7];
+	 ar[0]=101;
+	 ar[1]=102;
+	 ar[2]=103;
+	 ar[3]=104;
+	 ar[4]=105;
+	 ar[5]=106;
+	 ar[6]=107;
+
+	System.out.println(ar[0]);
+	}
+
+}
